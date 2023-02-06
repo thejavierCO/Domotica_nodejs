25e0369434eecd12dec4d86a7ab8b6282ea64ab4
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-02-06 04:46:12 -0600

    module db and IOT

--- src/db/index.js
+++ src/db/index.js
@@ -4,4 +4,38 @@ const supabase = createClient(
   process.env.SUPABASE_KEY,
 )
 
-module.exports = {supabase}
+class Db{
+  constructor(url,key){
+    this._db = createClient(url,key);
+  }
+  getTable(name){
+    return new Table(this._db.from(name));
+  }
+}
+
+class Table{
+  constructor(table){
+    this._table = table;
+    this._items = [];
+    this.getAllItems().then(e=>this._items = e);
+  }
+  async getAllItems(){
+    let {data,error} = await this._table.select('*')
+    if(error)throw error;
+    return data.map(e=>new Item(this,e))
+  }
+  async getItemsForColumn(name){
+    return this._items.map(e=>e[name])
+  }
+  async getItem(id){
+    return this._items.filter(e=>e.id == id);
+  }
+}
+
+class Item{
+  constructor(table,item){
+    this._table = table;
+    this._item = item;
+  }
+}
+module.exports = {Db}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
 const dotenv = require('dotenv')
 dotenv.config()
+const {Db} = require("./db/index.js");
+const {sinricpro,raiseEvent,startSinricPro,eventNames} = require("./IOT/index.js");
 
-const {sinricpro,raiseEvent,startSinricPro,eventNames} = require("./IOT");
-const {supabase} = require("./db");
+const db = new Db(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+)
+const Devices = db.getTable("devices")
+Devices.getItemsForColumn("devices_id").then(e=>{
+  console.log(e);
+}).catch(e=>console.log(e))
 
-const setPowerState = async (deviceid, data) => {
-  let {
-    data: [{ id }],
-    error: errorid,
-  } = await supabase
-    .from('devices')
-    .select('*')
-    .eq('device_id', deviceid)
+// const setPowerState = async (deviceid, data) => {
+//   let {
+//     data: [{ id }],
+//     error: errorid,
+//   } = await supabase
+//     .from('devices')
+//     .select('*')
+//     .eq('device_id', deviceid)
 
-  let { data: devices, error: errorUpdate } = await supabase
-    .from('devices')
-    .update([
-      {
-        id,
-        device_id: deviceid,
-        state: data == 'On' ? true : data == 'Off' ? false : false,
-      },
-    ])
-    .eq('device_id', deviceid)
+//   let { data: devices, error: errorUpdate } = await supabase
+//     .from('devices')
+//     .update([
+//       {
+//         id,
+//         device_id: deviceid,
+//         state: data == 'On' ? true : data == 'Off' ? false : false,
+//       },
+//     ])
+//     .eq('device_id', deviceid)
   
-  console.log(deviceid, data, devices, errorid, errorUpdate)
-}
+//   console.log(deviceid, data, devices, errorid, errorUpdate)
+// }
 
-(()=>{
-  console.log("Run");
-  startSinricPro(sinricpro, { setPowerState })
-})();
+// (async ()=>{
+//   // console.log("Run",Devices);
+//   // startSinricPro(sinricpro, { setPowerState })
+// })();
 
 //raiseEvent(sinricpro, eventNames.powerState, device1, { state: 'On' })
